8587772dd099e03e3f7840931c6577f90314b33c
Maxim Kokryashkin <max.kokryashkin@gmail.com>
2021-04-17 16:01:28 +0300

    done layout parsing

---
+++ layout-parser.py
@@ -0,0 +1,37 @@
+#!/bin/python
+from bs4 import BeautifulSoup as bs
+import lxml
+
+class Node:
+    def __init__(self, keys):
+        self.bounds = keys['bounds']
+        self.checkable = keys['checkable']
+        self.checked = keys['checked']
+        self.class_name = keys['class']
+        self.clickable = keys['clickable']
+        self.content_desc = keys['content-desc']
+        self.enabled = keys['enabled']
+        self.focusable = keys['focusable']
+        self.focused = keys['focused']
+        self.index = keys['index']
+        self.scrollable = keys['scrollable']
+        self.selected = keys['selected']
+        self.text = keys['text']
+
+        
+    
+
+with open('layout.xml', 'r') as layout:
+    content = layout.read()
+    bs_content = bs(content, 'lxml')
+
+    layout_nodes = bs_content.find_all('node')
+    
+    nodes = []
+    for node in layout_nodes:
+        nodes.append(Node(node))
+    
+    for node in nodes:
+        print(node.__dict__)
+
+        
