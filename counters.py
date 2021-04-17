f94a337686b6802b8bdb3131ef2232125bb39681
Maxim Kokryashkin <max.kokryashkin@gmail.com>
2021-04-18 02:34:13 +0300

    cv module changes

--- counters.py
+++ counters.py
@@ -1,6 +1,7 @@
 #!/bin/python
-from event_log_parser import parse_events
+from event_log_parser import parse_events, batch_events
 from layout_parser import collect_layout_nodes
+from glob import glob
 
 def check_inside(point, top_left, bottom_right):
     if top_left[0] <= point[0] and top_left[1] <= point[1]:
@@ -8,26 +9,36 @@ def check_inside(point, top_left, bottom_right):
             return True
     return False
 
-def collect_counters(layout_filename, event_log):
+def collect_counters(layouts, event_log):
     events = parse_events(event_log)
-    nodes = collect_layout_nodes(layout_filename)
+    nodes = []
+
+    for layout in layouts:
+        nodes.append(collect_layout_nodes(layout))
+    per_view_events = batch_events(1, len(layouts), events)
     
     counters = {}
 
-    for node in nodes:
-        counters[node.index] = {'type' : node.class_name, 'label' : node.text, 'events' : 0} 
+    for view in range(len(nodes)):
+        for node_idx in range(len(nodes[view])):
+            node = nodes[view][node_idx]
+            counters[str(view) + '.' + str(node_idx)] = {'type' : node.class_name, 'label' : node.text, 'events' : 0} 
 
-
-    for node in nodes:
-        for event in events:
-            event_point = (event.x, event.y)
-            if check_inside(event_point, node.top_left, node.bottom_right):
-                counters[node.index]['events'] += 1
+    
+    
+    for view_idx in range(len(per_view_events)):
+        for event in per_view_events[view_idx]:
+            for node_idx in range(len(nodes[view_idx])):
+                node = nodes[view_idx][node_idx]
+                event_point = (event.x, event.y)
+                if check_inside(event_point, node.top_left, node.bottom_right):
+                    counters[str(view_idx) + '.' + str(node_idx)]['events'] += 1
     
     return counters
    
 
-counters = collect_counters('layout.xml', 'event-log.txt')
+views = glob('telemetry/*.xml')
+counters = collect_counters(views, 'event-log.txt')
 
 for key in counters.keys():
     print('Id: ' + str(key) + ' ', end='')
--- cv_vizu.py
+++ cv_vizu.py
@@ -2,9 +2,11 @@
 
 from event_log_parser import parse_events, batch_events
 from layout_parser import collect_layout_nodes
+
+from glob import glob
 import cv2
 
-cap = cv2.VideoCapture('const.mp4')
+cap = cv2.VideoCapture('telemetry/const.mp4')
 fps = cap.get(cv2.CAP_PROP_FPS)
 frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
 print("Video runs at {} fps for {} frames".format(fps, frames))
@@ -16,13 +18,18 @@ events = parse_events('event-log.txt')
 batched = batch_events(fps, int(frames), events)
 current_frame = 21
 
-layout = collect_layout_nodes("layout.xml")
+xml_layouts = glob('telemetry/*.xml')
+layouts = []
+for xml in xml_layouts:
+    layouts.append(collect_layout_nodes(xml_layouts))
+
+cur_layout = 0
 
 while(cap.isOpened):
     ret, frame = cap.read()
     fixed = cv2.resize(frame, (1200, 2000), cv2.INTER_AREA)
 
-    for node in layout:
+    for node in layouts[cur_layout]:
         fixed = cv2.rectangle(fixed, tuple(node.top_left), tuple(node.bottom_right), (0, 255, 0), thickness=3)
     
     for event in batched[current_frame]:
@@ -36,6 +43,7 @@ while(cap.isOpened):
     # cv2.waitKey()
 
     current_frame += 1
+    cur_layout = current_frame // fps
 
 cap.release()
 out.release()
